ace339cba5e4bbd23ae64f7efba4b9784a3a6676
Youngjoon Kim <kimkingo@naver.com>
2016-01-16 06:28:52 +0000

    Initial commit

---
+++ ubuntu_init.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+#
+# Initialize development environment for Amazon Machine Image Ubuntu Server 14.04 LTS
+
+# packages
+sudo apt-get install git
+sudo apt-get install openjdk-7-jdk
+sudo apt-get install maven
+
+# vim & tmux
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp conf/tmux.conf ~/.tmux.conf
+cp conf/vimrc ~/.vimrc
+
+# git global configrations
+git config --global core.editor            vim
+git config --global core.pager             less
+git config --global color.ui               true
+git config --global color.status.added     yellow
+git config --global color.status.changed   green
+git config --global color.status.untracked cyan
+git config --global color.branch.remote    yellow
